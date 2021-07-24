60d7d9580a8c3dc3da18b39e7fc8e816f0c4fd2b
YutoKashiwagi <kashiwagi19950714@gmail.com>
2021-07-24 19:46:18 +0900

    キャッシュの実装

--- subject7/cache-app/index.js
+++ subject7/cache-app/index.js
@@ -4,12 +4,20 @@ const app = express()
 const port = 3000
 
 app.set('views', './views/')
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+
+// デフォルトの設定(デフォルトでキャッシュされる)
+app.use('/static/using-cache', express.static('public/using-cache'))
+
+// キャッシュをオフに
+app.use('/static/without-cache', express.static('public/without-cache', {
+  etag: false,
+  lastModified: false
+}))
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    apiOrigin
-  })
+  res.setHeader('Cache-Control', 'no-store') // ページのキャッシュをオフにする
+  res.render('index')
 })
 
 app.listen(port, () => {
