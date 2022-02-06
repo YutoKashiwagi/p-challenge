0aa9d872a753ed90f77c8ad6ef5170de3a23eaf3
YutoKashiwagi <kashiwagi19950714@gmail.com>
2022-02-06 14:44:14 +0900

    チュートリアル完成

--- subject10/my-app/src/index.js
+++ subject10/my-app/src/index.js
@@ -52,12 +52,20 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: ((step % 2) === 0)
+    })
+  }
+
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice();
 
@@ -72,14 +80,32 @@ class Game extends React.Component {
           squares: squares
         }]
       ),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const description = move ?
+        'Go to move #' + move :
+        'Go to game start'
+      
+      return(
+        <li key={move}>
+          <button
+            onClick={() => this.jumpTo(move)}
+          >
+            {description}
+          </button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -97,7 +123,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
