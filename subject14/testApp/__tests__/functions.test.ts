e70680082a7850cdc0d970f7ee7568b4506e048d
YutoKashiwagi <kashiwagi19950714@gmail.com>
2022-02-19 18:27:01 +0900

    add関数作成

--- subject14/testApp/__tests__/functions.test.ts
+++ subject14/testApp/__tests__/functions.test.ts
@@ -14,17 +14,26 @@ describe('add', () => {
   it('足算ができること', () => {
     expect(add(...[3, 10, 3])).toBe(16)
     expect(add(...[3.1, 10.1, 3.1])).toBe(16.3)
+  })
+
+  // 引数
+  it('引数は1個以上必要であること', () => {
+    expect(() => { add(...[])}).toThrow('引数は一つ以上指定してください')
     expect(add(...[3])).toBe(3)
-    expect(add(...[])).toBe(0)
   })
-  it('引数が30個の場合、計算できること', () => {
+  it('引数が30個以下でなければいけないこと', () => {
     expect(add(...Array(30).fill(1))).toBe(30)
+    expect( () => { add(...Array(31).fill(1)) }).toThrow('引数が多すぎます')
   })
-  it('引数が31個以上の場合、計算できないこと', () => {
-    expect(add(...Array(31).fill(1))).toBe('')
+
+  // 計算結果
+  it('計算結果は1000以下でなければいけないこと', () => {
+    expect(add(...Array(10).fill(100))).toBe(1000)
+    expect(() => {
+      const overThousand = [1000, 1]
+      add(...overThousand)
+    }).toThrow('too big')
   })
-  it.todo('引数が数字以外の場合、エラーになること')
-  it.todo('計算結果が1000より大きい場合、計算できないこと')
 })
 
 describe('subtract', () => {
--- subject14/testApp/functions.ts
+++ subject14/testApp/functions.ts
@@ -1,3 +1,14 @@
 export const add = (...args: number[]) => {
-  return args.reduce((sum, current) => sum + current, 0)
+  if (args.length === 0) {
+    throw new Error('引数は一つ以上指定してください')
+  }
+  if (args.length > 30) {
+    throw new Error('引数が多すぎます')
+  }
+
+  const result = args.reduce((sum, current) => sum + current, 0)
+  if (result > 1000) {
+    throw new Error('too big')
+  }
+  return result
 }
