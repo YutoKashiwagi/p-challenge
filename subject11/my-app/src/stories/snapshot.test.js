6c58556b7953320f2ccf4345de9dcd05dc6af687
YutoKashiwagi <kashiwagi19950714@gmail.com>
2022-03-06 20:34:47 +0900

    snapshotテスト作成

--- subject11/my-app/src/stories/snapshot.test.js
+++
@@ -1,7 +0,0 @@
-import initStoryshots from '@storybook/addon-storyshots';
-
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-
-initStoryshots({
-  test: imageSnapshot({})
-})
