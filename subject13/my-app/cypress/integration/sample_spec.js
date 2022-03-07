1f0bbca5c849e412624ae12bad881bf683bd59e2
YutoKashiwagi <kashiwagi19950714@gmail.com>
2022-03-07 22:56:01 +0900

    Xが勝つケースを追加

--- subject13/my-app/cypress/integration/sample_spec.js
+++ subject13/my-app/cypress/integration/sample_spec.js
@@ -3,4 +3,23 @@ describe('マルバツゲーム', () => {
     cy.visit('/')
     cy.contains('次のプレイヤー: X')
   })
+
+  it('Xが勝利すること', () => {
+    cy.visit('/')
+
+    cy.get('[data-qa=square0]').click()
+    cy.contains('次のプレイヤー: O')
+
+    cy.get('[data-qa=square1]').click()
+    cy.contains('次のプレイヤー: X')
+
+    cy.get('[data-qa=square3]').click()
+    cy.contains('次のプレイヤー: O')
+
+    cy.get('[data-qa=square4]').click()
+    cy.contains('次のプレイヤー: X')
+
+    cy.get('[data-qa=square6]').click()
+    cy.contains('Winner: X')
+  })
 })
